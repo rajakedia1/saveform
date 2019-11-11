1c36149168f8a57cb96749989b50269188e5229e
Raja Kumar Kedia <raja.kumarkedia@BBD-C02QQ5QNG8WN.local>
2019-11-11 18:06:35 +0530

    Added files

--- public/worker.js
+++ public/worker.js
@@ -9,7 +9,7 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   '/saveform/dist/bundle.js',
-  './', // Alias for index.html
+  '/saveform/', // Alias for index.html
 ];
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
